83b762439a150142971b7f4e6443e77059562ae2
Dominic Duncan <dominicduncan72@gmail.com>
2025-04-06 03:08:45 -0700

    risky commit

--- src/app.js
+++ src/app.js
@@ -8,8 +8,16 @@ const port = 3000;
 
 const __dirname_org = path.dirname(new URL(import.meta.url).pathname);
 const __dirname =  __dirname_org.substring(1);
-console.log(__dirname);
 const genAI = new GoogleGenerativeAI ('AIzaSyDTsyMVaXc8whJibBzyCLIT3lo08yGHKtQ');
+
+const stored_question_data = [];
+/*
+{
+ "uid":123,
+ "answer_index":n
+}
+
+*/
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -20,7 +28,7 @@ app.get('/', (req, res) => {
 app.get("/get_question", get_question_callback);
 
 async function get_question_callback(req,res){
-  
+
   let response;
   try{
     response = await createQuestion();
@@ -31,15 +39,38 @@ async function get_question_callback(req,res){
     res.json(response);
 }
 
+app.get("/check_answer", (req,res) => {
+
+  var query_uid = parseInt(req.query.question_uid);
+  var query_answer = req.query.answer;
+  let saved_answer = null;
+  for(let i=0;i<stored_question_data.length;i++){
+    if(stored_question_data[i]["uid"]===query_uid){
+      saved_answer = stored_question_data[i];
+    }
+  }
+  if (saved_answer == null){
+    res.json({"value":false});
+    return
+  }
+  if(saved_answer["answer_index"] == query_answer){
+    res.json({"value":true});
+    return
+  }
+  else{
+    res.json({"value":false});
+  }
+});
+
  function chooseimage(baseDir) {
 
-    const entries = fs.readdirSync(baseDir, { withFileTypes: true });             
+    const entries = fs.readdirSync(baseDir, { withFileTypes: true });
     const folders = entries.filter(entry => entry.isDirectory()).map(dir => dir.name);
     const randomFolder = folders[Math.floor(Math.random() * folders.length)];
 
     const pngfiles = fs.readdirSync(path.join(baseDir, randomFolder)).filter(file => file.endsWith('.png'));
     const randomFile = pngfiles[Math.floor(Math.random() * pngfiles.length)];
-    return [path.join(baseDir, randomFolder, randomFile), randomFolder, randomFile]; 
+    return [path.join(baseDir, randomFolder, randomFile), randomFolder, randomFile];
 }
 
 function fileToGenerativePart(path, mimeType) {
@@ -54,15 +85,15 @@ function fileToGenerativePart(path, mimeType) {
 async function passtogemini(imagePath) {
 
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-  
+
     const prompt = "approximate a equation to calculate the volume of the item in the picture! Only provide the final equation in the response. Use the naming convention as letter_ number. in the form v=. !!!!dont provide any information regarding what is in the picture!!! In latex text!!!";
-  
+
     const imageParts = [
       fileToGenerativePart(imagePath, "image/png"),
     ];
-  
+
     const generatedContent = await model.generateContent([prompt, ...imageParts]);
-    
+
     const result = (generatedContent.response.text());
     return result;
   }
@@ -70,9 +101,15 @@ async function passtogemini(imagePath) {
 
 async function createQuestion() {
     const selectedImage = chooseimage(path.join(__dirname, '../public/assets'));
-    const latexEquation = await passtogemini(selectedImage[0]);
+    const latexEquation = "$$ \sum_{i=1}^{n} i^2 = \frac{n(n+1)(2n+1)}{6} $$";///await passtogemini(selectedImage[0]);
     const imglink = selectedImage[1]+ "/" +selectedImage[2];
-    console.log(imglink); 
+
+    let uid = Math.floor((Math.random()*100)+1);
+    let correctanswer = 1;
+    stored_question_data.push({
+      "uid":uid,
+      "answer_index":1
+    });
 
     return ({
       "img_1":imglink,
@@ -80,7 +117,7 @@ async function createQuestion() {
       "img_3":imglink,
       "img_4":imglink,
       "equation":"$$"+latexEquation+"$$",//the back slashes must be double backslashes
-      "uid":123
+      "uid":uid
     });
 }
 
